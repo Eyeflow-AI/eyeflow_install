8d08342cc242dcf8a25770232ab0c41c81d415a5
Alex Sobral de Freitas <alexsobral@siliconlife.ai>
2024-12-11 19:11:26 +0000

    Fix in run_flow

--- run_flow_monitor.sh
+++ run_flow_monitor.sh
@@ -3,5 +3,5 @@
 export LD_LIBRARY_PATH=/opt/eyeflow/lib:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export CUDA_MODULE_LOADING=LAZY
 
-sudo python3 prepare_models.py
-sudo ./eyeflow_edge --monitor
+python3 prepare_models.py
+./eyeflow_edge --monitor
