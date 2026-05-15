5932c2d93528e8ffdaa3bb3c73ce68d6d6b34e1a
Alex Sobral de Freitas <alexsobral@siliconlife.ai>
2026-05-15 14:11:23 +0000

    Update libcudnn package names in installation script

--- setup_edge_x86-local.sh
+++ setup_edge_x86-local.sh
@@ -31,7 +31,7 @@ then
   dpkg -i cuda-keyring_1.1-1_all.deb
   apt update
   apt-get install -y cuda-toolkit-12-4
-  apt-get install -y libcudnn libcudnn-dev
+  apt-get install -y libcudnn8 libcudnn8-dev
 fi
 
 # install python
